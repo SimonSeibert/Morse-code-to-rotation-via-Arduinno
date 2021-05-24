4d0f5fa9eeceb1bab8235f02911cda51d36aa124
SimonSeibert <simonseibert1998@gmail.com>
2021-05-24 12:40:50 +0200

    Ouija Board can be used now, Planchette will move

---
+++ Assets/Scripts/MovePlanchette.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlanchette : MonoBehaviour
+{
+    public float moveTime = 1f;
+    public Transform pos_a, pos_b, pos_c, pos_d, pos_e, pos_f, pos_0, pos_1, pos_2, pos_3, pos_4, pos_5, pos_6, pos_7, pos_8, pos_9;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Transform[] pos =
+        {
+            pos_a, pos_b, pos_c, pos_d, pos_e, pos_f, pos_0, pos_1, pos_2, pos_3, pos_4, pos_5, pos_6, pos_7, pos_8, pos_9
+        };
+        moveToMultiple(pos);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void moveTo(Transform pos, float delay)
+    {
+        LeanTween.move(gameObject, pos, moveTime).setEaseInOutExpo().setDelay(delay);
+    }
+
+    public void moveToMultiple(Transform[] positions)
+    {
+        for (int i = 0; i < positions.Length; i++)
+        {
+            moveTo(positions[i], i * moveTime);
+        }
+    }
+}
