b752e2ff9a5eecf7d7d73acb9910dfb56732cccb
SimonSeibert <simonseibert1998@gmail.com>
2021-05-26 14:13:27 +0200

    Planchette is now more precise

--- Assets/Scripts/MovePlanchette.cs
+++ Assets/Scripts/MovePlanchette.cs
@@ -7,10 +7,11 @@ public class MovePlanchette : MonoBehaviour
 {
     public float moveTime = 1f;
     public Transform pos_a, pos_b, pos_c, pos_d, pos_e, pos_f, pos_0, pos_1, pos_2, pos_3, pos_4, pos_5, pos_6, pos_7, pos_8, pos_9;
-    private Transform targetTmp = null;
+    private Transform targetTmp;
 
     void Start()
     {
+        targetTmp = transform;
         Transform[] pos =
         {
             pos_a, pos_3, pos_b, pos_f, pos_d, pos_e, pos_c, pos_0, pos_1, pos_2, pos_9, pos_4, pos_5, pos_6, pos_7, pos_8
