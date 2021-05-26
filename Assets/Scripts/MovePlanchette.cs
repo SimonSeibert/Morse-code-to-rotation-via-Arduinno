e9988c59587bf18f3a66943deeb872306cc405f0
SimonSeibert <simonseibert1998@gmail.com>
2021-05-26 14:04:53 +0200

    Removed LeanTween and added own smooth movement

--- Assets/Scripts/MovePlanchette.cs
+++ Assets/Scripts/MovePlanchette.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,7 +7,8 @@ public class MovePlanchette : MonoBehaviour
 {
     public float moveTime = 1f;
     public Transform pos_a, pos_b, pos_c, pos_d, pos_e, pos_f, pos_0, pos_1, pos_2, pos_3, pos_4, pos_5, pos_6, pos_7, pos_8, pos_9;
-    // Start is called before the first frame update
+    private Transform targetTmp = null;
+
     void Start()
     {
         Transform[] pos =
@@ -16,22 +18,22 @@ public class MovePlanchette : MonoBehaviour
         moveToMultiple(pos);
     }
 
-    // Update is called once per frame
     void Update()
     {
-
-    }
-
-    public void moveTo(Transform pos, float delay)
-    {
-        LeanTween.move(gameObject, pos, moveTime).setEaseInOutExpo().setDelay(delay);
+        transform.position = Vector3.Lerp(transform.position, targetTmp.position, (moveTime + 2f) * Time.deltaTime);
     }
 
     public void moveToMultiple(Transform[] positions)
     {
         for (int i = 0; i < positions.Length; i++)
         {
-            moveTo(positions[i], i * moveTime);
+            StartCoroutine("moveTo", (positions[i], i * moveTime));
         }
+    }
+
+    IEnumerator moveTo((Transform, float) parameter)
+    {
+        yield return new WaitForSeconds(parameter.Item2);
+        targetTmp = parameter.Item1;
     }
 }
