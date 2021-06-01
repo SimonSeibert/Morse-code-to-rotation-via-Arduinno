edb6e71379d8107f2d9efafe7e202d2ea1dd96ff
SimonSeibert <simonseibert1998@gmail.com>
2021-06-01 11:08:11 +0200

    More comments in code

--- Assets/Scripts/ArduinoCommunicator.cs
+++ Assets/Scripts/ArduinoCommunicator.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class ArduinoCommunicator : MonoBehaviour
 {
     public int comPort = 4;
+    public int baudRate = 19200;
     SerialPort serial;
     string receivedString;
 
@@ -14,38 +15,56 @@ public class ArduinoCommunicator : MonoBehaviour
 
     void Update()
     {
+        //By pressing "1" you can receive a message from the Arduino
         if (Input.GetKeyDown(KeyCode.Alpha2))
         {
             receiveMessageFromArdu();
         }
+        //By pressing "2" you can send a message from the Arduino
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            sendMessageToArdu();
+            sendMessageToArdu("blink\n");
         }
     }
 
+    /// <summary>
+    /// Close the serial port so it can be used again by an other application
+    /// </summary>
     private void OnApplicationQuit()
     {
         serial.Close();
     }
 
+    /// <summary>
+    /// Read from the serial port and log the message
+    /// </summary>
     void receiveMessageFromArdu()
     {
         receivedString = serial.ReadLine();
         Debug.Log(receivedString);
     }
 
-    void sendMessageToArdu()
+    /// <summary>
+    /// Write a message in the serial port which the Arduino can read
+    /// </summary>
+    /// <param name="s">Message you want to send</param>
+    void sendMessageToArdu(string s)
     {
-        serial.Write("blink\n");
+        serial.Write(s);
     }
 
+    /// <summary>
+    /// Connects to the specified serial port
+    /// </summary>
     void setup()
     {
-        serial = new SerialPort("COM" + comPort, 19200);
+        //Defina a new serial port with the com port and baud rate
+        serial = new SerialPort("COM" + comPort, baudRate);
 
+        //Small timeout between read accesses
         serial.ReadTimeout = 16;
 
+        //If the port isn't open -> open it
         if (serial.IsOpen == false)
         {
             serial.Open();
@@ -56,6 +75,7 @@ public class ArduinoCommunicator : MonoBehaviour
             Debug.Log("COM" + comPort + " is already open...");
         }
 
+        //I somehow have to write something in the serial port so that the application doesn't crash...
         serial.NewLine = "\n";
         serial.Write("something" + "\n");
     }
--- Assets/Scripts/MovePlanchette.cs
+++ Assets/Scripts/MovePlanchette.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 
 public class MovePlanchette : MonoBehaviour
 {
+    [Tooltip("Time the planchette needs to move between hexa numbers")]
     public float moveTime = 1f;
+    [Tooltip("Positions of all hexa numbers")]
     public Transform pos_a, pos_b, pos_c, pos_d, pos_e, pos_f, pos_0, pos_1, pos_2, pos_3, pos_4, pos_5, pos_6, pos_7, pos_8, pos_9;
-    private Transform targetTmp;
+
+    private Transform targetTmp; // Current target of the planchette
 
     void Start()
-    {
-        targetTmp = transform;
+    {     
+        targetTmp = transform; //At the beginning the planchette targets itself -> it doesn't move 
         Transform[] pos =
         {
             pos_a, pos_3, pos_b, pos_f, pos_d, pos_e, pos_c, pos_0, pos_1, pos_2, pos_9, pos_4, pos_5, pos_6, pos_7, pos_8
@@ -21,20 +24,31 @@ public class MovePlanchette : MonoBehaviour
 
     void Update()
     {
+        // Planchette always moves to it's target in a smooth way
         transform.position = Vector3.Lerp(transform.position, targetTmp.position, (moveTime + 2f) * Time.deltaTime);
     }
 
+    /// <summary>
+    /// Moves the planchette to multiple targets
+    /// </summary>
+    /// <param name="positions"></param>
     public void moveToMultiple(Transform[] positions)
     {
         for (int i = 0; i < positions.Length; i++)
         {
+            // The delay is calculated in a way so the planchette starts moving to the hexa numbers one after another
             StartCoroutine("moveTo", (positions[i], i * moveTime));
         }
     }
 
+    /// <summary>
+    /// Moves the planchette to a target after a specified delay
+    /// </summary>
+    /// <param name="parameter">Transform is the new target, float is the delay after which the movement will start</param>
+    /// <returns></returns>
     IEnumerator moveTo((Transform, float) parameter)
     {
-        yield return new WaitForSeconds(parameter.Item2);
+        yield return new WaitForSeconds(parameter.Item2); // Waits a specified amount of time before moving
         targetTmp = parameter.Item1;
     }
 }
