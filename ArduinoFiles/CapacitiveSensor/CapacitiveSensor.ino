62625560be4dd272b0ad9b92e393476a16cc3c87
SimonSeibert <simonseibert1998@gmail.com>
2021-06-06 14:15:22 +0200

    LED morse output done

--- ArduinoFiles/CapacitiveSensor/CapacitiveSensor.ino
+++ ArduinoFiles/CapacitiveSensor/CapacitiveSensor.ino
@@ -1,16 +1,15 @@
 #include <CapacitiveSensor.h>
 
 CapacitiveSensor Sensor = CapacitiveSensor(2, 4);
-long val;
-int state;
-const int threshhold = 400;
+long val;                         // Value from capacitive reading
+int state;                        // State to allow press and release function to be executed 1 time after another
+const int threshhold = 400;       // Threshold for when capacitive reading reading counts as a press or a release
 float timestampOnPress = 0;       // timestampOnPress is used to get presstime
 float timestampOnRelease = 0;     // timestampOnRelease is used to get releasetime
 
 
 void setup() {
   Serial.begin(9600);
-  pinMode(LED_BUILTIN, OUTPUT);
   state = 0;
 }
 
@@ -19,9 +18,7 @@ void loop() {
   //Serial.println(val);
 
   //PRESS
-  if (val >= threshhold && state == 0) {
-    digitalWrite(LED_BUILTIN, HIGH);
-    
+  if (val >= threshhold && state == 0) {   
     timestampOnPress = millis();                        //Gets time since execution start and current time in ms
     float releaseTime = millis() - timestampOnRelease;  //get release time in ms by subtracting the timestamp from the current time
 
@@ -32,16 +29,15 @@ void loop() {
   }
   //RELEASE
   else if (val < threshhold && state == 1) {
-    digitalWrite(LED_BUILTIN, LOW);
-
-    timestampOnRelease = millis();
-    float pressTime = millis() - timestampOnPress;
+    timestampOnRelease = millis();                      
+    float pressTime = millis() - timestampOnPress;      //get press time in ms by subtracting the timestamp from the current time
     
     Serial.print("DIY Sensor released. Press time was: ");
     Serial.println(pressTime);
     
     state = 0;
   }
-  delay(50);
+  
+  delay(50); //Delay between readings
 }
 
--- ArduinoFiles/MorseOutput/MorseOutput.ino
+++ ArduinoFiles/MorseOutput/MorseOutput.ino
@@ -1,81 +1,102 @@
 // constants won't change. They're used here to set pin numbers:
-const int buttonPin = 14;     // the number of the pushbutton pin
-const int ledPin =  16;      // the number of the LED pin
-const int sd = 250;
-const int ld = 750;
-const int pd = 250;
+const int ledPin =  16;       // the number of the LED pin
+
+int dot = 250;
+int dash;
+int betweenSymbols;
+int betweenLetters;
+int betweenWords;
+
 
 // variables will change:
-int buttonState = 0;         // variable for reading the pushbutton status
+int buttonState = 0;          // variable for reading the pushbutton status
 
 void setup() {
   // initialize the LED pin as an output:
   pinMode(ledPin, OUTPUT);
-  // initialize the pushbutton pin as an input:
-  pinMode(buttonPin, INPUT);
-}
+  //For Debugging
+  Serial.begin(9600);
 
-//True is short, false is long
-void blink(bool isShort) {
-  if(isShort) {
-    digitalWrite(ledPin, HIGH);
-    delay(sd);
-    digitalWrite(ledPin, LOW);
-    delay(pd);
-  } else {
-    digitalWrite(ledPin, HIGH);
-    delay(ld);
-    digitalWrite(ledPin, LOW);
-    delay(pd);
-  }
-}
+  //Morse Alphabet Times
+  dash = dot * 3;
+  betweenSymbols = dot;
+  betweenLetters = dot * 3;
+  betweenWords = dot * 7;
 
-//Morse a letter, TODO: Add all letters
-void morse(char c) {
-  switch(c) {
-    case 'S':
-      blink(true);
-      blink(true);
-      blink(true);
-      break;
-    case 'I':
-      blink(true);
-      blink(true);
-      break;
-    case 'M':
-      blink(false);
-      blink(false);
-      break;
-    case 'O':
-      blink(false);
-      blink(false);
-      blink(false);
-      break;
-    case 'N':
-      blink(false);
-      blink(true);
-      break;
-  }
-}
-
-void morseSimon() {
-  morse('S');
-  morse('I');
-  morse('M');
-  morse('O');
-  morse('N');
+  //Test
+  morseSentence("Das ist ein Test");
 }
 
 void loop() {
-  // read the state of the pushbutton value:
-  buttonState = digitalRead(buttonPin);
+}
 
-  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
-  if (buttonState == HIGH) {
-    // blink 'Simon' in morse Code:
-    morseSimon();
-  } else {
-    // turn LED off:
-    digitalWrite(ledPin, LOW);
+void morseSentence(char *sentence) {
+  int i = 0;
+  while (sentence[i] != '\0') {
+    Serial.print("Now morseing letter '"); Serial.print(sentence[i]); Serial.println("'");
+    morse(sentence[i]);
+    i++;
   }
+}
+
+//isShort: True is dot, false is dash
+void blink(bool isShort) {
+  if (isShort) {
+    digitalWrite(ledPin, HIGH);
+    delay(dot);
+    digitalWrite(ledPin, LOW);
+    delay(betweenSymbols);
+  } else {
+    digitalWrite(ledPin, HIGH);
+    delay(dash);
+    digitalWrite(ledPin, LOW);
+    delay(betweenSymbols);
+  }
+}
+
+// Letter to corresponding morse time
+void morse(char c) {
+  switch (c) {
+    case 'A': case 'a': blink(true); blink(false); break;
+    case 'B': case 'b': blink(false); blink(true); blink(true); blink(true); break;
+    case 'C': case 'c': blink(false); blink(true); blink(false); blink(true); break;
+    case 'D': case 'd': blink(false); blink(true); blink(true); break;
+    case 'E': case 'e': blink(true); break;
+    case 'F': case 'f': blink(true); blink(true); blink(false); blink(true); break;
+    case 'G': case 'g': blink(false); blink(false); blink(true); break;
+    case 'H': case 'h': blink(true); blink(true); blink(true); blink(true); break;
+    case 'I': case 'i': blink(true); blink(true); break;
+    case 'J': case 'j': blink(true); blink(false); blink(false); blink(false); break;
+    case 'K': case 'k': blink(false); blink(true); blink(false); break;
+    case 'L': case 'l': blink(true); blink(false); blink(true); blink(true); break;
+    case 'M': case 'm': blink(false); blink(false); break;
+    case 'N': case 'n': blink(false); blink(true); break;
+    case 'O': case 'o': blink(false); blink(false); blink(false); break;
+    case 'P': case 'p': blink(true); blink(false); blink(false); blink(true); break;
+    case 'Q': case 'q': blink(false); blink(false); blink(true); blink(false); break;
+    case 'R': case 'r': blink(true); blink(false); blink(true); break;
+    case 'S': case 's': blink(true); blink(true); blink(true); break;
+    case 'T': case 't': blink(false); break;
+    case 'U': case 'u': blink(true); blink(true); blink(false); break;
+    case 'V': case 'v': blink(true); blink(true); blink(true); blink(false); break;
+    case 'W': case 'w': blink(true); blink(false); blink(false); break;
+    case 'X': case 'x': blink(false); blink(true); blink(true); blink(false); break;
+    case 'Y': case 'y': blink(false); blink(true); blink(false); blink(false); break;
+    case 'Z': case 'z': blink(false); blink(false); blink(true); blink(true); break;
+    case '1': blink(true); blink(false); blink(false); blink(false); blink(false); break;
+    case '2': blink(true); blink(true); blink(false); blink(false); blink(false); break;
+    case '3': blink(true); blink(true); blink(true); blink(false); blink(false); break;
+    case '4': blink(true); blink(true); blink(true); blink(true); blink(false); break;
+    case '5': blink(true); blink(true); blink(true); blink(true); blink(true); break;
+    case '6': blink(false); blink(true); blink(true); blink(true); blink(true); break;
+    case '7': blink(false); blink(false); blink(true); blink(true); blink(true); break;
+    case '8': blink(false); blink(false); blink(false); blink(true); blink(true); break;
+    case '9': blink(false); blink(false); blink(false); blink(false); blink(true); break;
+    case '0': blink(false); blink(false); blink(false); blink(false); blink(false); break;
+    case ' ': delay(betweenWords - betweenLetters - betweenSymbols); break; // Time between words is calculated like this because of the following calculation behind the switch case.
+    default: Serial.println("Char not supported for LED output!"); break;
+  }
+
+  //After that the end of a letter is reached. The remaining pause time is calculated like this:
+  delay(betweenLetters - betweenSymbols);
 }
